174ef2bd3df88cdafe2e897f072d42f8ff62f88b
Aleksandr Titov <26012167+AleksandrTitov@users.noreply.github.com>
2023-05-11 17:58:22 +0300

    fix: Fix conditions for outputs (#4)

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,34 @@
 output "id" {
   description = "Specifies a resource ID in UUID format"
-  value       = var.rocketmq_instance_id == null ? null : huaweicloud_dms_rocketmq_instance.main.0.id
+  value       = var.rocketmq_instance_id == null ? huaweicloud_dms_rocketmq_instance.main.0.id : null
 }
 
 output "status" {
   description = "Indicates the status of the DMS RocketMQ instance"
-  value       = var.rocketmq_instance_id == null ? null : huaweicloud_dms_rocketmq_instance.main.0.status
+  value       = var.rocketmq_instance_id == null ? huaweicloud_dms_rocketmq_instance.main.0.status : null
 }
 
 output "type" {
   description = "Indicates the DMS RocketMQ instance type"
-  value       = var.rocketmq_instance_id == null ? null : huaweicloud_dms_rocketmq_instance.main.0.type
+  value       = var.rocketmq_instance_id == null ? huaweicloud_dms_rocketmq_instance.main.0.type : null
 }
 
 output "specification" {
   description = "Indicates the instance specification"
-  value       = var.rocketmq_instance_id == null ? null : huaweicloud_dms_rocketmq_instance.main.0.specification
+  value       = var.rocketmq_instance_id == null ? huaweicloud_dms_rocketmq_instance.main.0.specification : null
 }
 
 output "node_num" {
   description = "Indicates the node quantity"
-  value       = var.rocketmq_instance_id == null ? null : huaweicloud_dms_rocketmq_instance.main.0.node_num
+  value       = var.rocketmq_instance_id == null ? huaweicloud_dms_rocketmq_instance.main.0.node_num : null
 }
 
 output "broker_address" {
   description = "Indicates the service data address"
-  value       = var.rocketmq_instance_id == null ? null : huaweicloud_dms_rocketmq_instance.main.0.broker_address
+  value       = var.rocketmq_instance_id == null ? huaweicloud_dms_rocketmq_instance.main.0.broker_address : null
 }
 
 output "namesrv_address" {
   description = "Indicates the metadata address"
-  value       = var.rocketmq_instance_id == null ? null : huaweicloud_dms_rocketmq_instance.main.0.namesrv_address
+  value       = var.rocketmq_instance_id == null ? huaweicloud_dms_rocketmq_instance.main.0.namesrv_address : null
 }
